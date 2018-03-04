95b4e9451a39a6b67f23821b1de4b192edd8240d
Unknown <sandhiya24.developer@gmail.com>
2018-03-04 22:26:31 +0530

    [#Added go routine]

--- Services/Ping.go
+++ Services/Ping.go
@@ -7,9 +7,13 @@ import (
 	"log"
 	"net"
 	"net/http"
+	//"os"
 	"os/exec"
 	"strings"
 	"time"
+
+	//"github.com/sendgrid/sendgrid-go"
+	//"github.com/sendgrid/sendgrid-go/helpers/mail"
 )
 
 type Ping struct {
@@ -85,18 +89,29 @@ func HostPing(w http.ResponseWriter, r *http.Request) {
 	w.Write(Senddata)
 
 }
-func heartBeatmonthly(ip string, Email string) {
+func heartBeatmonthly(ip string, Email string, ticker *time.Ticker) (int, int) {
 	log.Println("Pinging------")
+	var Monthlypass int = 0
+	var Monthlyfail int = 0
 
-	for range time.Tick(time.Second * 2) {
+	for range ticker.C {
 		out, _ := exec.Command("ping", ip, "-c 5", "-i 3", "-w 10").Output()
 		//out, _ := exec.Command("ping", ip).Output()
 		if strings.Contains(string(out), "Destination Host Unreachable") {
 			fmt.Println(ip, "-TANGO DOWN send email to", Email)
+			Monthlyfail = Monthlyfail + 1
+
 		} else {
 			fmt.Println(ip, "IT'S ALIVEEE")
+			Monthlypass = Monthlypass + 1
+			fmt.Println(Monthlypass)
+
 		}
+
 	}
+
+	return Monthlypass, Monthlyfail
+
 }
 func Monitorip(w http.ResponseWriter, r *http.Request) {
 	var GetIpvalue Monitor
@@ -106,8 +121,68 @@ func Monitorip(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("Error on Get particular details", err)
 	}
 	Db.Insertip(GetIpvalue.Ip, GetIpvalue.Mailid)
+	ticker := time.NewTicker(2 * time.Second)
+	var getpass int
+	var getfails int
 
-	go heartBeatmonthly(GetIpvalue.Ip, GetIpvalue.Mailid)
-	time.Sleep(time.Second * 100)
+	go func() {
+		getpass, getfails = heartBeatmonthly(GetIpvalue.Ip, GetIpvalue.Mailid, ticker)
+
+	}()
+
+	time.Sleep(time.Second * 10)
+	fmt.Println("loop finished ", GetIpvalue.Ip, GetIpvalue.Mailid)
+	fmt.Println("got pass and fail count  ", getpass, getfails)
+	//SendReport("Monthly", GetIpvalue.Ip, GetIpvalue.Mailid)
+	ticker.Stop()
+
+}
+func SendReport(Monthly string, Monthlypassreport int, monthylyfail int, addr string, number string) {
+	var htmlContent string
+	//	sendkey := os.Getenv("SENDGRID_API_KEYGO")
+	//	from := mail.NewEmail("E3 Shopping", "sandhiyabalakrishnan6@gmail.com")
+	//	subject := "E3 NOTIFICATION - New Order Recieved!"
+	//	to := mail.NewEmail("Foxutech Tool", "sandhiyabalakrishnan6@gmail.com")
+	//	plainTextContent := "sandhiyabalakrishnan6@gmail.com"
+
+	switch Monthly {
+	case "Monthly":
+
+		htmlContent = "<div><b style='font-size:15px'>: </b></div><br> " +
+			"<div style='font-style:sans-serif'>LOGINID - " +
+			"<div style='font-style:sans-serif'>ADDRESS - " +
+			"</div><div style='font-style:sans-serif'>DATE -" +
+			"</div><div style='font-style:sans-serif'>TOTAL AMOUNT -" +
+			"</div><div style='font-style:sans-serif'>NO OF PRODUCTS -" +
+			"</div><table class='table' border='1' style='padding:5px;font-style:sans-serif'><tbody >" + "<tr style='border-bottom:1pt solid black;'><th >PRODUCT</th><th>RATE</th><th>WEIGHT</th></tr>" +
+			"</tbody></table><br><div>Please Check E3 Admin Panel for more detail ...!</div>"
+
+	case "Failure":
+
+		htmlContent = "<div><b style='font-size:15px'>E3 NEW ORDER : </b></div><br> " +
+			"<div style='font-style:sans-serif'>NAME  - " +
+			"<div style='font-style:sans-serif'>ADDRESS - " +
+			"</div><div style='font-style:sans-serif'>NUMBER -" +
+			"</div><div style='font-style:sans-serif'>PRODUCT  -" +
+			"</div><div style='font-style:sans-serif'>MESSAGE" +
+			"</div></tbody></table><br><div>Please Check E3 Admin Panel for more detail ...!</div>"
+
+	default:
+		fmt.Println("default case")
+
+	}
+	fmt.Println(htmlContent)
+	//message := mail.NewSingleEmail(from, subject, to, plainTextContent, htmlContent)
+
+	//	client := sendgrid.NewSendClient(sendkey)
+	//	response, err := client.Send(message)
+	//	if err != nil {
+	//		fmt.Println(err)
+	//	} else {
+	//		fmt.Println(response.StatusCode)
+	//		fmt.Println(response.Body)
+	//		fmt.Println(response.Headers)
+
+	//	}
 
 }
